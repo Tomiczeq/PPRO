697027a47ae513c5e7aebb42ec67a9be53eb4065
Tomiczeq <kratochviltomas12@gmail.com>
2021-11-10 20:08:58 +0100

    feat: config, dont run server with 'flask run'

--- monitored_app/run.sh
+++ monitored_app/run.sh
@@ -1,6 +1,1 @@
-#tell Flask where to find application
-export FLASK_APP=src/server
-#specify run mode
-export FLASK_ENV=development
-
-flask run
+python3 src/server.py --conf=conf/server.conf
--- monitored_app/src/server.py
+++ monitored_app/src/server.py
@@ -1,52 +1,59 @@
 
 import time
+import argparse
+import configparser
 import prometheus_client
 from flask import Flask
 from flask import request
 from flask import Response
 
 
-from metrics import REQUEST_COUNT
+from prometheus_metrics import Metrics
 
+
+parser = argparse.ArgumentParser(description='Sample app')
+parser.add_argument("--conf", action="store",
+                    default="conf/server.conf")
 
 app = Flask(__name__)
 CONTENT_TYPE_LATEST = "text/plain; version=0.0.4; charset=utf-8"
 
 
-def start_timer():
-    request.start_time = time.time()
-
-
-def stop_timer(response):
-    resp_time = time.time() - request.start_time
-    print("Response time: %ss\n" % resp_time)
-    REQUEST_COUNT.labels("test", request.method,
-                         request.path, response.status).inc()
-    return response
-
-
 def record_request_data(response):
     print("Request path: %s Request method: %s Response status: %s\n" %
           (request.path, request.method, response.status_code))
+    metrics.request_count_total(
+            method=request.method,
+            endpoint=request.path,
+            status=response.status_code
+    )
     return response
 
 
 def setup_metrics(app):
-    app.before_request(start_timer)
-    # The order here matters since we want stop_timer
-    # to be executed first
     app.after_request(record_request_data)
-    app.after_request(stop_timer)
 
 
 @app.route("/")
 def hello():
     return f"hello from {__name__}!"
 
+
 @app.route('/metrics/')
-def metrics():
+def get_metrics():
     return Response(prometheus_client.generate_latest(),
                     mimetype=CONTENT_TYPE_LATEST)
 
 
-setup_metrics(app)
+if __name__ == "__main__":
+    setup_metrics(app)
+    app_name = "test"
+    metrics = Metrics(app_name)
+
+    args = parser.parse_args()
+    config = configparser.ConfigParser()
+    config.read(args.conf)
+    port = config.getint("server", "port", fallback="8015")
+    debug = config.getboolean("server", "debug", fallback=True)
+
+    app.run(port=port, debug=True)

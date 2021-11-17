81e385110a9351870d0041762123c6714031ceac
Tomiczeq <kratochviltomas12@gmail.com>
2021-11-17 16:10:50 +0100

    feat: request_duration_seconds and errors_total metrics

--- components/monitored_app/src/prometheus_metrics.py
+++ components/monitored_app/src/prometheus_metrics.py
@@ -1,17 +1,29 @@
-from prometheus_client import Counter, Histogram
+from prometheus_client import Counter
+from prometheus_client import Gauge
+from prometheus_client import Histogram
 
 
+# TODO mozna pouzit zabudovane decoratory
 class Metrics:
 
     def __init__(self, app_name):
-        self.REQUEST_COUNT = Counter(
-                'request_count', 'App Request Count',
-                ['app', 'method', 'endpoint', 'status']
+        self.HISTOGRAM = Histogram(
+                'request_duration_seconds', 'track requests duration',
+                ['app', 'method', 'endpoint', 'status'],
+                buckets=[0.01, 0.05, 0.1, 0.5]
+        )
+        self.COUNTER = Counter(
+                'errors_total', 'count number of errors',
+                ['app', 'type']
         )
         self.app_name = app_name
 
-    def request_count_total(self, method, endpoint, status,
-                            increment=1):
-        (self.REQUEST_COUNT
+    def request_duration_seconds(self, method, endpoint, status, duration):
+
+        (self.HISTOGRAM
              .labels(self.app_name, method, endpoint, status)
-             .inc(increment))
+             .observe(duration))
+
+    def errors_total(self, err_type, increment=1):
+
+        self.COUNTER.labels(self.app_name, err_type).inc(increment)
--- components/monitored_app/src/server.py
+++ components/monitored_app/src/server.py
@@ -1,54 +1,80 @@
 import time
+import logging
+import random
 import argparse
 import configparser
 import prometheus_client
+
+import numpy as np
+
 from flask import Flask
 from flask import request
 from flask import Response
+from flask import g
 
 
 from prometheus_metrics import Metrics
 
 
-parser = argparse.ArgumentParser(description='Sample app')
+parser = argparse.ArgumentParser(description="Sample app")
 parser.add_argument("--conf", action="store",
                     default="conf/server.conf")
 
 app = Flask(__name__)
 CONTENT_TYPE_LATEST = "text/plain; version=0.0.4; charset=utf-8"
+EXCEPTION_LIST = ["KeyError", "AttributeError", "RuntimeError", "TypeError"]
 
 
-def record_request_data(response):
-    print("Request path: %s Request method: %s Response status: %s\n" %
-          (request.path, request.method, response.status_code))
-    metrics.request_count_total(
+@app.before_request
+def before_request():
+    g.start_time = time.time()
+
+
+@app.after_request
+def after_request(response):
+    duration = time.time() - g.start_time
+    app.logger.info(f"Request on endpoint {request.path} "
+                    f"took {duration} sec.")
+    metrics.request_duration_seconds(
             method=request.method,
             endpoint=request.path,
-            status=response.status_code
+            status=response.status_code,
+            duration=duration
     )
     return response
 
 
-def setup_metrics(app):
-    app.after_request(record_request_data)
+def get_sleep_time():
+    return np.random.exponential(scale)
 
 
-@app.route("/")
-def hello():
-    return f"hello from {__name__}!"
+@app.route("/", defaults={"path": ""}, methods=["GET"])
+@app.route("/<path:path>", methods=["GET", "POST"])
+def simulation(path):
+
+    # simulate some processing time
+    time.sleep(get_sleep_time())
+
+    # simulate some errors
+    if random.random() < error_chance:
+        exception = random.choice(EXCEPTION_LIST)
+        metrics.errors_total(err_type=exception)
+
+    return Response(status=200)
 
 
-@app.route('/metrics')
-@app.route('/metrics/')
+@app.route("/metrics")
+@app.route("/metrics/")
 def get_metrics():
     return Response(prometheus_client.generate_latest(),
                     mimetype=CONTENT_TYPE_LATEST)
 
 
 if __name__ == "__main__":
-    setup_metrics(app)
-    app_name = "test"
-    metrics = Metrics(app_name)
+
+    # disable flask messages
+    log = logging.getLogger("werkzeug")
+    log.disabled = True
 
     args = parser.parse_args()
     config = configparser.ConfigParser()
@@ -56,5 +82,15 @@ if __name__ == "__main__":
     port = config.getint("server", "port", fallback=8015)
     host = config.get("server", "host", fallback="0.0.0.0")
     debug = config.getboolean("server", "debug", fallback=True)
+
+    # simulation constants
+    scale = config.getfloat(
+            "simulation", "duration", fallback=0.05)
+    error_chance = config.getfloat(
+            "simulation", "error_chance", fallback=0.1)
+
+    # init metrics
+    app_name = config.get("metrics", "app_name", fallback="test")
+    metrics = Metrics(app_name)
 
     app.run(host=host, port=port, debug=True)

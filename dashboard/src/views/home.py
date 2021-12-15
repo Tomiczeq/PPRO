9765b402afafbacd3e2330ea0a75598b9db35f6e
Tomiczeq <kratochviltomas12@gmail.com>
2021-12-15 22:11:49 +0100

    feat: simple navbar

--- dashboard/src/views/home.py
+++ dashboard/src/views/home.py
@@ -1,3 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint
+from flask import render_template
 
-home = Blueprint('home', __name__)
+home = Blueprint('home', __name__,
+                 template_folder="../templates/home")
+
+
+@home.route("/", methods=["GET"])
+def homepage():
+    return render_template("home.html")

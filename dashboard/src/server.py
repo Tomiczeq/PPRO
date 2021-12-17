0cfd92cb13fdea1b99a5f890e3d5d81465dd9dc1
Tomiczeq <kratochviltomas12@gmail.com>
2021-12-17 01:16:45 +0100

    chore: add some todo

--- dashboard/src/server.py
+++ dashboard/src/server.py
@@ -30,6 +30,7 @@ if __name__ == "__main__":
 
     app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://@localhost/test"
 
+    # TODO
     with app.app_context():
         db.create_all()
         sentinel = Dashboard('sentinel')

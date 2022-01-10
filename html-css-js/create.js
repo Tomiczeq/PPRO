6f37d6cedb0e17f095d0d22e5f8ae7d9e039cb58
Tomiczeq <kratochviltomas12@gmail.com>
2022-01-10 20:01:37 +0100

    clean up

--- html-css-js/create.js
+++
@@ -1,4 +0,0 @@
-var create_dashboard_btn = document.querySelector("#create_dashboard_btn");
-create_dashboard_btn.addEventListener("click", () => {
-    $('.main_container').load('dashboard.html');
-})
